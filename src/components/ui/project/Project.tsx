2974bad63f5450bebb32df80260a31c12ec285da
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2021-05-27 20:10:15 +0100

    add responsive to project section

--- src/components/ui/project/Project.tsx
+++ src/components/ui/project/Project.tsx
@@ -11,18 +11,10 @@ const Projects = () => {
           <div className={style.imageContainer}>
             <img
               className={style.image}
-              src={`https://picsum.photos/id/${121 + index}/300/2000`}
+              src={`https://picsum.photos/id/${121 + index}/400/2000`}
               ref={imageRef}
             />
           </div>
-          {/* <div className={style.content}>
-            <h1>title</h1>
-            <p>description</p>
-            <div className={style.buttonWrapper}>
-              <button>See More</button>
-              <button>Code Source</button>
-            </div>
-          </div> */}
         </div>
       ))}
     </div>
