b3f49bbafaf7b363b6fdaa0d2dfb9ea49d1ae45e
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2021-05-29 03:50:04 +0100

    add tutorial by slug

--- src/pages/tutorial/[slug].tsx
+++ src/pages/tutorial/[slug].tsx
@@ -1,12 +1,44 @@
-import React from 'react';
-import { Layout } from '@components/core';
+import { MDXRemote } from 'next-mdx-remote';
 
-const slug = () => {
+import { getFiles, getFileBySlug } from '@lib/mdx';
+import { getTweets } from '@lib/twitter';
+import { Layout } from '@components/core';
+import Image from 'next/image';
+import { ReadingLayout } from '@components/core';
+
+export default function Blog({ mdxSource, frontMatter }) {
   return (
     <Layout>
-      <div>Hello</div>
+      <ReadingLayout {...frontMatter}>
+        <MDXRemote components={{ Image }} {...mdxSource} />
+      </ReadingLayout>
     </Layout>
   );
+}
+
+export const getStaticProps = async (context) => {
+  const { slug } = context.params;
+  const post = await getFileBySlug('blog', slug);
+
+  return { props: { ...post } };
 };
 
-export default slug;
+export async function getStaticPaths() {
+  const posts = await getFiles('blog');
+
+  return {
+    paths: [
+      {
+        params: {
+          slug: 'preview',
+        },
+      },
+    ],
+    paths: posts.map((p) => ({
+      params: {
+        slug: p.replace(/\.mdx/, ''),
+      },
+    })),
+    fallback: false,
+  };
+}
