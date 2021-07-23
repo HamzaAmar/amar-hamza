6f8a1adc27ab6ee5a1e0dc4b770e49276a5818c6
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2021-07-23 19:53:06 +0100

    add some style to skills and cursor

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -6,7 +6,7 @@ export { default as Skill } from './skill';
 export { default as Social } from './social';
 export { default as CircleProgress } from './progress';
 export { default as MDX } from './mdx';
-export { Button, Input, TextArea } from './form';
+export { Button, Input, Textarea } from './form';
 export { default as Switcher } from './switcherMode';
 export { default as Loading } from './loading';
 export { default as Cursor } from './cursor';
