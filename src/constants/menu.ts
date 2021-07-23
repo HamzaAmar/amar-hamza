c10e4714ead4da654b75070df631e1aec052809a
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2021-07-23 19:56:33 +0100

    change the name of menu TO MENU because is constants

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -1,8 +1,14 @@
-const MenuData = [
+interface Menu {
+  id: number;
+  name: string;
+  path: string;
+}
+
+const MENU: Menu[] = [
   { id: 1, name: 'Home', path: '/' },
   { id: 2, name: 'Blog', path: '/blogs' },
   { id: 3, name: 'Tutorial', path: '/tutorial' },
   { id: 4, name: 'Challange', path: '/challenge' },
   { id: 5, name: 'Contact', path: '/contact' },
 ];
-export default MenuData;
+export default MENU;
