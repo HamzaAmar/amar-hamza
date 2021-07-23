d25cb98ed8049d0f51945c1270f56d853a8c9887
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2021-07-23 19:49:30 +0100

    add social link to about

--- src/components/ui/about/About.tsx
+++ src/components/ui/about/About.tsx
@@ -1,74 +1,3 @@
-// import React, { ReactNode } from 'react';
-
-// import style from './About.module.css';
-// import cn from 'classnames';
-// import {
-//   Facebook,
-//   Github,
-//   Linkdin,
-//   Twitter,
-// } from '@components/icons';
-// import { Button } from '../form';
-// import facebook from '@components/icons/facebook';
-
-// interface IProps {
-//   name: string;
-//   icon: ReactNode;
-// }
-
-// const item = [
-//   { id: 1, icon: <Facebook />, name: 'Facebook' },
-//   { id: 2, icon: <Twitter />, name: 'Twitter' },
-//   { id: 3, icon: <Linkdin />, name: 'Linkdin' },
-//   { id: 4, icon: <Github />, name: 'Github' },
-// ];
-
-// const Item = ({ name, icon }: IProps) => {
-//   return (
-//     <li className={style.articleItem}>
-//       <div className={style.iconContainer}>
-//         <div className={style.mediaIconContainer}>{icon}</div>
-//       </div>
-//       <div className={style.articleMediaContainer}>
-//         <p>{name}</p>
-//       </div>
-//     </li>
-//   );
-// };
-
-// const About = () => {
-//   return (
-//     <div className={style.container}>
-//       <div className={cn(style.GreetingName, style.article)}>
-//         <div> Hi , i am </div>
-//         <h1 className={style.myName}>Hamza Amar</h1>
-//       </div>
-//       <div className={cn(style.myDescription, style.article)}>
-//         <p className={style.paragraphDescription}>
-//           i am a full stack javascript developer am good at react ,
-//           javascript , css , nodeJs
-//         </p>
-//       </div>
-//       <div className={cn(style.infoContent, style.article)}>
-//         <img src="https://picsum.photos/id/111/300/300" />{' '}
-//       </div>
-//       <div className={cn(style.media, style.article)}>
-//         <ul className={style.articleItems}>
-//           {item.map(({ id, ...rest }) => (
-//             <Item key={id} {...rest} />
-//           ))}
-//         </ul>
-//       </div>
-
-//       <div className={cn(style.connect, style.article)}>
-//         <Button size="fluid">see My CV</Button>
-//         <Button size="fluid">Download CV</Button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default About;
 import React from 'react';
 import styles from './About.module.css';
 import {
@@ -76,6 +5,7 @@ import {
   Github,
   Linkdin,
   Twitter,
+  Instagram,
 } from '@components/icons';
 import { Button } from '../form';
 
@@ -83,13 +13,41 @@ interface Socials {
   id: number;
   icon: React.ReactNode;
   name: string;
+  link: string;
 }
 
 const socials: Socials[] = [
-  { id: 1, icon: <Facebook />, name: 'Facebook' },
-  { id: 2, icon: <Twitter />, name: 'Twitter' },
-  { id: 3, icon: <Linkdin />, name: 'Linkdin' },
-  { id: 4, icon: <Github />, name: 'Github' },
+  {
+    id: 1,
+    icon: <Facebook />,
+    name: 'Facebook',
+    link: 'https://www.facebook.com',
+  },
+
+  {
+    id: 2,
+    icon: <Twitter />,
+    name: 'Twitter',
+    link: 'https://twitter.com/HamzaMiloudAma1',
+  },
+  {
+    id: 3,
+    icon: <Instagram />,
+    name: 'Instagram',
+    link: 'https://twitter.com/HamzaMiloudAma1',
+  },
+  {
+    id: 4,
+    icon: <Linkdin />,
+    name: 'Linkdin',
+    link: 'https://www.linkedin.com/in/hamza-miloud-amar-463b24167',
+  },
+  {
+    id: 5,
+    icon: <Github />,
+    name: 'Github',
+    link: 'https://github.com/HamzaAmar',
+  },
 ];
 
 const About = () => {
@@ -123,10 +81,15 @@ const About = () => {
         <p className={styles.rightTitle}>scroll Down</p>
       </div>
       <ul className={styles.socials}>
-        {socials.map(({ id, icon }) => (
-          <li className={styles.socialItem} key={id}>
-            {icon}
-          </li>
+        {socials.map(({ id, icon, link }) => (
+          <a
+            href={link}
+            target="_blank"
+            className={styles.socialItem}
+            key={id}
+          >
+            <li>{icon}</li>
+          </a>
         ))}
       </ul>
     </div>
