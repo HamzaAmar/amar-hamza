db7c2a6be14ed7d62f4320c5f11b3ef537fe1383
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2021-08-03 17:23:02 +0100

    reduce css in skill component section

--- src/components/ui/skill/skill.tsx
+++ src/components/ui/skill/skill.tsx
@@ -13,43 +13,32 @@ interface IList {
 const SkillsContainer = () => {
   return (
     <div className={style.container}>
-      {(skills as [[string, IList[]]]).map(([skill, lists]) => {
-        return (
-          <div className={style.skill} key={skill}>
-            <h1 className={style.skillTitle}> {skill} </h1>
-            <ul className={style.skillsList}>
-              {lists.map((list) => {
-                return (
-                  <li className={style.skillItem} key={list.title}>
-                    <div className={style.skillContainer}>
-                      {list.icon ? (
-                        <div className={style.iconContainer}>
-                          <div className={style.icon}>
-                            {list.icon}
-                          </div>
-                        </div>
-                      ) : null}
-                      <h2 className={style.skillIconTitle}>
-                        {list.title}
-                      </h2>
-                    </div>
-                    <div className={style.progressContainer}>
-                      <CircleProgress
-                        width="100%"
-                        height="100%"
-                        color="orange"
-                        lvl={list.level}
-                      >
-                        <tspan>{list.level}%</tspan>
-                      </CircleProgress>
-                    </div>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        );
-      })}
+      <ul className={style.skillsList}>
+        {skills.map((list) => {
+          return (
+            <li className={style.skillItem} key={list.title}>
+              <div className={style.skillContainer}>
+                {list.icon ? (
+                  <div className={style.iconContainer}>
+                    <div className={style.icon}>{list.icon}</div>
+                  </div>
+                ) : null}
+                <h2 className={style.skillIconTitle}>{list.title}</h2>
+              </div>
+              <div className={style.progressContainer}>
+                <CircleProgress
+                  width="100%"
+                  height="100%"
+                  color="orange"
+                  lvl={list.level}
+                >
+                  <tspan>{list.level}%</tspan>
+                </CircleProgress>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
