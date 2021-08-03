cfb6702f4231c984e2550569ffa0cbe2720c6d10
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2021-08-03 17:24:17 +0100

    change skills shape

--- src/constants/skills.tsx
+++ src/constants/skills.tsx
@@ -3,76 +3,48 @@ import {
   ReactIcon,
   NextIcon,
   TypeScript,
-  Node,
   HTML,
   CSS,
   Graphql,
-  Mongodb,
-  Express,
   Gatsby,
 } from '@components/icons';
 const skills = [
-  [
-    'Front End',
-    [
-      {
-        title: 'React',
-        level: 95,
-        icon: <ReactIcon width={10} height={10} />,
-      },
-      {
-        title: 'NextJs',
-        level: 95,
-        icon: <NextIcon />,
-      },
-      {
-        title: 'typeScript',
-        level: 85,
-        icon: <TypeScript />,
-      },
-      {
-        title: 'HTML',
-        level: 95,
-        icon: <HTML />,
-      },
+  {
+    title: 'React',
+    level: 95,
+    icon: <ReactIcon width={10} height={10} />,
+  },
+  {
+    title: 'NextJs',
+    level: 95,
+    icon: <NextIcon />,
+  },
+  {
+    title: 'typeScript',
+    level: 85,
+    icon: <TypeScript />,
+  },
+  {
+    title: 'HTML',
+    level: 95,
+    icon: <HTML />,
+  },
 
-      {
-        title: 'CSS',
-        level: 95,
-        icon: <CSS />,
-      },
-      {
-        title: 'GraphQL',
-        level: 70,
-        icon: <Graphql />,
-      },
-      {
-        title: 'Gatsby',
-        level: 80,
-        icon: <Gatsby />,
-      },
-    ],
-  ],
-  [
-    'Back End',
-    [
-      {
-        title: 'Node',
-        level: 80,
-        icon: <Node />,
-      },
-      {
-        title: 'Mongo DB',
-        level: 80,
-        icon: <Mongodb />,
-      },
-      {
-        title: 'Express',
-        level: 70,
-        icon: <Express />,
-      },
-    ],
-  ],
+  {
+    title: 'CSS',
+    level: 95,
+    icon: <CSS />,
+  },
+  {
+    title: 'GraphQL',
+    level: 70,
+    icon: <Graphql />,
+  },
+  {
+    title: 'Gatsby',
+    level: 80,
+    icon: <Gatsby />,
+  },
 ];
 
 export default skills;
