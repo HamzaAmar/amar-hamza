916cf349a56d225b6d8fc54e2b9225480e1fe26e
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2021-08-07 02:36:28 +0100

    add new skills

--- src/constants/skills.tsx
+++ src/constants/skills.tsx
@@ -7,43 +7,60 @@ import {
   CSS,
   Graphql,
   Gatsby,
+  Node,Express,Mongodb
 } from '@components/icons';
 const skills = [
   {
     title: 'React',
-    level: 95,
+    level: 90,
     icon: <ReactIcon width={10} height={10} />,
   },
   {
     title: 'NextJs',
-    level: 95,
+    level: 90,
     icon: <NextIcon />,
   },
   {
     title: 'typeScript',
-    level: 85,
+    level: 90,
     icon: <TypeScript />,
   },
   {
     title: 'HTML',
-    level: 95,
+    level: 90,
     icon: <HTML />,
   },
 
   {
     title: 'CSS',
-    level: 95,
+    level: 90,
     icon: <CSS />,
+  },
+   {
+    title: 'Gatsby',
+    level: 80,
+    icon: <Gatsby />,
   },
   {
     title: 'GraphQL',
     level: 70,
     icon: <Graphql />,
   },
+ 
+   {
+    title: 'Node js',
+    level: 60,
+    icon: <Node />,
+  },
   {
-    title: 'Gatsby',
-    level: 80,
-    icon: <Gatsby />,
+    title: 'Express',
+    level: 60,
+    icon: <Express />,
+  },
+  {
+    title: 'Mongo',
+    level: 60,
+    icon: <Mongodb />,
   },
 ];
 
