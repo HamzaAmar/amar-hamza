873b905385723cbf028da20a6be61b5305f0bdba
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2021-09-05 13:04:07 +0100

    remove mdx from index.ts components ui

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -5,7 +5,6 @@ export { default as Service } from './service';
 export { default as Skill } from './skill';
 export { default as Social } from './social';
 export { default as CircleProgress } from './progress';
-export { default as MDX } from './mdx';
 export { Button, Input, Textarea } from './form';
 export { default as Switcher } from './switcherMode';
 export { default as Loading } from './loading';
