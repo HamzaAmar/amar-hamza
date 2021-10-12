4b91120e0a743f57a2195ef204d17ffd9602ef82
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2021-10-12 03:12:22 +0100

    add social data

---
+++ src/constants/socialMenu.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+  Facebook,
+  Github,
+  Linkdin,
+  Twitter,
+  Instagram,
+} from '@components/icons';
+
+interface Socials {
+  id: number;
+  icon: React.ReactNode;
+  name: string;
+  link: string;
+}
+
+const SOCIALS: Socials[] = [
+  {
+    id: 1,
+    icon: <Facebook />,
+    name: 'Facebook',
+    link: 'https://www.facebook.com',
+  },
+
+  {
+    id: 2,
+    icon: <Twitter />,
+    name: 'Twitter',
+    link: 'https://twitter.com/HamzaMiloudAma1',
+  },
+  {
+    id: 3,
+    icon: <Instagram />,
+    name: 'Instagram',
+    link: 'https://twitter.com/HamzaMiloudAma1',
+  },
+  {
+    id: 4,
+    icon: <Linkdin />,
+    name: 'Linkdin',
+    link: 'https://www.linkedin.com/in/hamza-miloud-amar-463b24167',
+  },
+  {
+    id: 5,
+    icon: <Github />,
+    name: 'Github',
+    link: 'https://github.com/HamzaAmar',
+  },
+];
+
+export default SOCIALS;
