5e5d6933557c37295d6be5691786df5ed3799d74
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2021-10-12 03:09:57 +0100

    add article component

--- src/pages/blogs/index.tsx
+++ src/pages/blogs/index.tsx
@@ -1,14 +1,9 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState } from 'react';
+import React from 'react';
 import { getAllFilesFrontMatter } from '@lib/mdx';
 import { Layout } from '@components/core';
-// import { Article } from '@components/ui';
-import styles from '@styles/blog.module.css';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { formatDate } from '@utils/formatDate';
 import { GetStaticProps } from 'next';
-import Image from 'next/image';
+import { Article } from '@components/ui';
+import PostType from '@type/post';
 
 export const getStaticProps: GetStaticProps = () => {
   const posts = getAllFilesFrontMatter('blog');
@@ -16,72 +11,10 @@ export const getStaticProps: GetStaticProps = () => {
   return { props: { posts } };
 };
 
-interface BlogProps {
-  // author: { name: string; picture: string };
-  // coverImage: string;
-  date: string;
-  excerpt: string;
-  image: string;
-  slug: string;
-  // tag: string;
-  title: string;
-  tags: string[];
-}
-
-const Article = ({
-  image,
-  excerpt,
-  title,
-  date,
-  slug,
-  tags,
-}: BlogProps) => {
-  const { pathname } = useRouter();
-
-  return (
-    <article className={styles.article}>
-      <div className={styles.contentImage}>
-        <Link href={`${pathname}/${slug}`}>
-          <a className={styles.href}>
-            <div className={styles.imageContainer}>
-              <Image
-                src="/assets/blog/hello-world/cover.jpg"
-                alt="Hello world"
-                objectFit="cover"
-                layout="fill"
-                className={styles.cover}
-                objectPosition="center top"
-              />
-            </div>
-          </a>
-        </Link>
-        <div className={styles.tagContainer}>
-          {tags.map((tag) => (
-            <div key={tag} className={styles.tag}>
-              {tag}
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className={styles.contentInfo}>
-        <time dateTime={date} className={styles.date}>
-          {formatDate(date)}
-        </time>
-        <Link href={`${pathname}/${slug}`}>
-          <h1>
-            <a className={styles.title}>{title}</a>
-          </h1>
-        </Link>
-        <p className={styles.description}>{excerpt}</p>
-      </div>
-    </article>
-  );
-};
-
-export default function Blog({ posts }: { posts: BlogProps[] }) {
+export default function Blog({ posts }: { posts: PostType[] }) {
   return (
     <Layout title="Blog Page">
-      <div className={styles.content}>
+      <div className="articleContainer">
         {posts.map((post) => (
           <Article key={post.slug} {...post} />
         ))}
--- src/pages/challenge/index.tsx
+++ src/pages/challenge/index.tsx
@@ -1,76 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { getAllFilesFrontMatter } from '@lib/mdx';
 import { Layout } from '@components/core';
-// import { Article } from '@components/ui';
-import styles from '@styles/blog.module.css';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { formatDate } from '@utils/formatDate';
 import { GetStaticProps } from 'next';
+import { Article } from '@components/ui';
+import PostType from '@type/post';
 
 export const getStaticProps: GetStaticProps = () => {
   const challenges = getAllFilesFrontMatter('challenge');
   return { props: { challenges } };
 };
 
-interface ChallangeProps {
-  // author: { name: string; picture: string };
-  // coverImage: string;
-  date: string;
-  excerpt: string;
-  image: string;
-  slug: string;
-  // tag?: string;
-  title: string;
-}
-
-const Article = ({
-  image,
-  excerpt,
-  title,
-  date,
-  slug,
-}: ChallangeProps) => {
-  const { pathname } = useRouter();
-
-  return (
-    <Link href={`${pathname}/${slug}`}>
-      {/* <a className={styles.href}> */}
-      <article className={styles.article}>
-        <div className={styles.contentImage}>
-          <div className={styles.imageContainer}>
-            <img
-              className={styles.image}
-              src="https://picsum.photos/id/200/1000/1000"
-              alt=""
-            />
-            <div className={styles.tagContainer}>
-              <div className={styles.tag}>Hello</div>
-              <div className={styles.tag}>Hello</div>
-              <div className={styles.tag}>Hello</div>
-              <div className={styles.tag}>Hello</div>
-            </div>
-          </div>
-        </div>
-        <div className={styles.contentInfo}>
-          <small className={styles.date}>{formatDate(date)}</small>
-          <h1 className={styles.title}>{title}</h1>
-          <p className={styles.description}>{excerpt}</p>
-        </div>
-      </article>
-      {/* </a> */}
-    </Link>
-  );
-};
-
 export default function Blog({
   challenges,
 }: {
-  challenges: ChallangeProps[];
+  challenges: PostType[];
 }) {
   return (
     <Layout title="Challenge Page">
-      <div className={styles.content}>
+      <div className="articleContainer">
         {challenges.map((post) => (
           <Article key={post.slug} {...post} />
         ))}
