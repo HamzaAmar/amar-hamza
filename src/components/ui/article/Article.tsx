6d24a06a23dfe95f94f0d703b5f800eb6fc587ee
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2021-10-12 03:01:24 +0100

    add article components

--- src/components/ui/article/Article.tsx
+++ src/components/ui/article/Article.tsx
@@ -1,57 +1,50 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import Link from 'next/link';
-import cn from 'classnames';
+import { formatDate } from '@utils/formatDate';
+import Image from 'next/image';
+import PostType from '@type/post';
+
+import { Button } from '..';
 
 import styles from './Article.module.css';
 
-interface ArticleProps {
-  title: string;
-  slug: string;
-  date: string;
-  excerpt: string;
-  tags: string[];
-  coverImage: string;
-  className: string;
-}
-
-const Article = ({
-  title,
-  slug,
-  date,
-  excerpt,
-  tags,
-  coverImage,
-  className,
-}: ArticleProps) => {
+const Article = ({ excerpt, title, date, slug, tags }: PostType) => {
   return (
-    <Link href={`/blogs/${slug}`}>
-      <a className={styles.href}>
-        <div className={cn(styles.container, className)}>
-          <img src={coverImage} alt="" />
-          <h1 className={styles.title}>{title}</h1>
-          <h2 className={styles.date}>
-            {date}
-            <span role="img" aria-label="cup of coffee">
-              ☕️
-            </span>
-            5 min read
-          </h2>
-          <p className={styles.text}>{excerpt}</p>
-          <nav className={styles.nav}>
-            <ul>
-              {tags.map((keyword) => (
-                <li key={keyword}>
-                  <Link href={`/tags/${keyword.toLowerCase()}`}>
-                    {keyword}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
+    <article className={styles.article}>
+      <div className={styles.contentImage}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="/assets/blog/hello-world/cover.jpg"
+            alt="Hello world"
+            objectFit="cover"
+            layout="fill"
+            className={styles.cover}
+            objectPosition="center top"
+          />
         </div>
-      </a>
-    </Link>
+
+        <div className={styles.tagContainer}>
+          {tags?.map((tag) => (
+            <div key={tag} className={styles.tag}>
+              {tag}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className={styles.contentInfo}>
+        <time dateTime={date} className={styles.date}>
+          {formatDate(date)}
+        </time>
+        <h1 className={styles.title}>{title}</h1>
+        <p className={styles.description}>{excerpt}</p>
+      </div>
+
+      <Link href={`/blogs/${slug}`} passHref>
+        <Button variant="outline" as="a" className={styles.button}>
+          See More
+        </Button>
+      </Link>
+    </article>
   );
 };
 
