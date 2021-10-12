c50565c8a1b4f18b8a660a333e9de7726824af1b
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2021-10-12 03:00:11 +0100

    change social to it components and and language container to the react and css and html in the about area

--- src/components/ui/about/About.tsx
+++ src/components/ui/about/About.tsx
@@ -1,84 +1,26 @@
 import React from 'react';
-import {
-  Facebook,
-  Github,
-  Linkdin,
-  Twitter,
-  Instagram,
-} from '@components/icons';
+import { Social } from '@components/ui';
 
 import { Button } from '../form';
 
 import styles from './About.module.css';
 
-interface Socials {
-  id: number;
-  icon: React.ReactNode;
-  name: string;
-  link: string;
-}
-
-const socials: Socials[] = [
-  {
-    id: 1,
-    icon: <Facebook />,
-    name: 'Facebook',
-    link: 'https://www.facebook.com',
-  },
-
-  {
-    id: 2,
-    icon: <Twitter />,
-    name: 'Twitter',
-    link: 'https://twitter.com/HamzaMiloudAma1',
-  },
-  {
-    id: 3,
-    icon: <Instagram />,
-    name: 'Instagram',
-    link: 'https://twitter.com/HamzaMiloudAma1',
-  },
-  {
-    id: 4,
-    icon: <Linkdin />,
-    name: 'Linkdin',
-    link: 'https://www.linkedin.com/in/hamza-miloud-amar-463b24167',
-  },
-  {
-    id: 5,
-    icon: <Github />,
-    name: 'Github',
-    link: 'https://github.com/HamzaAmar',
-  },
-];
-
 const About = () => {
   return (
     <div className={styles.container}>
-      <ul className={styles.socials}>
-        {socials.map(({ id, icon, link, name }) => (
-          <li key={id} className={styles.socialItem}>
-            <a
-              href={link}
-              target="_blank"
-              className={styles.socialItemLink}
-              rel="noreferrer"
-              aria-label={`${name} icon`}
-            >
-              {icon}
-            </a>
-          </li>
-        ))}
-      </ul>
+      {/* Social Section */}
+      <Social className={styles.absolute} direction="vertical" />
       <div className={styles.heroContent}>
         <div className={styles.description}>
           <h1 className={styles.myName}>Hamza Amar</h1>
           <p className={styles.info}>
             Senior Front End Developer I like every think about it but
             I am in love with
-            <strong className={styles.strong}>React</strong>
-            <strong className={styles.strong}>HTML</strong>
-            <strong className={styles.strong}>CSS</strong>
+            <span className={styles.language}>
+              <strong className={styles.strong}>React</strong>
+              <strong className={styles.strong}>HTML</strong>
+              <strong className={styles.strong}>CSS</strong>
+            </span>
           </p>
           <div className={styles.buttonContainer}>
             <Button>Hire Me</Button>
