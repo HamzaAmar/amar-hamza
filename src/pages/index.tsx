18d80f6d6ed588d27268984d04eb733a60d1e619
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2021-10-12 03:07:58 +0100

    add article to the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,24 @@
 import styles from '@styles/home.module.css';
 import React from 'react';
-import { Service, Project, About, Skill } from '@components/ui';
+import {
+  Service,
+  Project,
+  About,
+  Skill,
+  Article,
+} from '@components/ui';
 import { Layout } from '@components/core';
+import { getAllFilesFrontMatter } from '@lib/mdx';
+import { GetStaticProps } from 'next';
+import PostType from '@type/post';
 
-const IndexPage = () => {
+export const getStaticProps: GetStaticProps = () => {
+  const posts = getAllFilesFrontMatter('blog', 6);
+
+  return { props: { posts } };
+};
+
+const IndexPage = ({ posts }: { posts: PostType[] }) => {
   return (
     <>
       <Layout title="Home Page">
@@ -18,6 +33,14 @@ const IndexPage = () => {
           <section className={styles.section}>
             <h1 className={styles.heading}>Skills</h1>
             <Skill />
+          </section>
+          <section className={styles.section}>
+            <h1 className={styles.heading}>Articles</h1>
+            <div className="articleContainer">
+              {posts.map((props) => (
+                <Article key={props.slug} {...props} />
+              ))}
+            </div>
           </section>
           <section className={styles.section}>
             <h1 className={styles.heading}>Projects</h1>
