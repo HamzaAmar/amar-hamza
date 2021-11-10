9cf6b87982cb823105fa2d563a8dfbeac69b0e5f
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2021-11-10 18:08:30 +0100

    change the nale of curriculum_vitea to resume

--- src/pages/curriculum_vitae.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const curriculumVitae = () => {
-  return <div>curriculum vitae</div>;
-};
-
-export default curriculumVitae;
---
+++ src/pages/resume.tsx
@@ -0,0 +1,12 @@
+import { Layout } from '@components/core';
+import React from 'react';
+
+const curriculumVitae = () => {
+  return (
+    <Layout>
+      <h1>Resume</h1>
+    </Layout>
+  );
+};
+
+export default curriculumVitae;
