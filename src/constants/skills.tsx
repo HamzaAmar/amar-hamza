a545d37f3ceb3bf4d0570495c49513af2ff32ec8
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2022-08-01 02:55:19 +0100

    refactor: add slug property to the object for the key in the map

--- src/constants/skills.tsx
+++ src/constants/skills.tsx
@@ -10,60 +10,60 @@ import {
   Node,
   Express,
   Mongodb,
+  Remix,
 } from '@components/icons';
 
 const skills = [
   {
     title: 'React',
+    slug: 'react',
     level: 90,
-    icon: <ReactIcon width={10} height={10} />,
+    icon: (
+      <ReactIcon width={10} height={10} fill="var(--color-text)" />
+    ),
   },
   {
     title: 'NextJs',
+    slug: 'next_js',
     level: 90,
     icon: <NextIcon />,
   },
   {
     title: 'typeScript',
+    slug: 'typeScript',
     level: 90,
     icon: <TypeScript />,
   },
   {
     title: 'HTML',
+    slug: 'html',
     level: 90,
     icon: <Html />,
   },
 
   {
     title: 'CSS',
+    slug: 'css',
     level: 90,
     icon: <Css />,
   },
   {
     title: 'Gatsby',
+    slug: 'gatsby',
     level: 80,
     icon: <Gatsby />,
   },
   {
-    title: 'GraphQL',
-    level: 70,
-    icon: <Graphql />,
+    title: 'Remix',
+    slug: 'remix',
+    level: 80,
+    icon: <Remix />,
   },
-
   {
     title: 'Node js',
+    slug: 'node_js',
     level: 60,
     icon: <Node />,
-  },
-  {
-    title: 'Express',
-    level: 60,
-    icon: <Express />,
-  },
-  {
-    title: 'Mongo',
-    level: 60,
-    icon: <Mongodb />,
   },
 ];
 
