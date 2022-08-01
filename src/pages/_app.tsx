a7408115c0d697b8ebeb052dea1a4f3ed4d42f77
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2022-08-01 02:47:11 +0100

    refactor: remove css and add scss style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { AppProps } from 'next/app';
-import '@styles/global.css';
+import '@scss/main.scss';
 import { useRouter } from 'next/router';
 import { ThemeProvider } from 'next-themes';
 
