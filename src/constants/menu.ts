b9ae7f5c9b3c792051d9253dbf37243072fc1678
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2022-08-01 02:25:28 +0100

    fix: remove tutorial from menu constant file

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -7,7 +7,6 @@ interface Menu {
 const MENU: Menu[] = [
   { id: 1, name: 'Home', path: '/' },
   { id: 2, name: 'Blog', path: '/blogs' },
-  { id: 3, name: 'Tutorial', path: '/tutorial' },
   { id: 4, name: 'Challange', path: '/challenge' },
   { id: 5, name: 'Contact', path: '/contact' },
 ];
