035b328e99df9caa3d45b65c87dfd46d7de61fa4
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2023-02-09 19:20:08 +0100

    refactor: footer component   - refactor footer components   - fix merge conflict problem

--- src/components/common/footer/footer.tsx
+++ src/components/common/footer/footer.tsx
@@ -1,27 +1,49 @@
+import { Flex, Grid, Heading, Text } from '@components/core';
 import React from 'react';
 
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="footer--contact">
-        <h2>Thank you</h2>
-        <p>
-          Hello thank you for the visit if you like my website and you
-          want to see more staff about me please follow me in social
-          media
-        </p>
-      </div>
+      <Grid
+        columns="1fr 1fr"
+        className="footer--hero-content md_grid-one"
+      >
+        <Flex
+          items="center"
+          justify="center"
+          direction="column"
+          className="footer--contact l_box"
+        >
+          <Heading>Thank you</Heading>
+          <Text color="slate" contrast="low" align="center">
+            Hello thank you for the visit if you like my website and
+            you want to see more staff about me please follow me in
+            social media
+          </Text>
+        </Flex>
 
-      <div className="footer--subscription">
-        <h2>Join the Newsletter</h2>
-        <p>Please subscribe to our web site to see all new stuff</p>
-      </div>
-      <div className="footer--copy-right">
-        <p>
+        <Flex
+          items="center"
+          justify="center"
+          direction="column"
+          className="footer--subscription"
+        >
+          <Heading>Join the Newsletter</Heading>
+          <Text color="slate" contrast="low" align="center">
+            Please subscribe to our web site to see all new stuff
+          </Text>
+        </Flex>
+      </Grid>
+      <Flex
+        justify="center"
+        items="center"
+        className="footer--copy-right"
+      >
+        <Text>
           © {new Date().getFullYear()} Hamza Miloud Amar. All Rights
           Reserved
-        </p>
-      </div>
+        </Text>
+      </Flex>
     </div>
   );
 };
--- src/pages/blogs/index.tsx
+++ src/pages/blogs/index.tsx
@@ -3,7 +3,6 @@
 import React from 'react';
 import { Layout } from '@components/common';
 import { GetStaticProps } from 'next';
-// import { Article } from '@components/ui';
 import { compareDesc } from 'date-fns';
 import { Articles } from '@components/sections';
 import { allBlogs, type Blog } from 'contentlayer/generated';
