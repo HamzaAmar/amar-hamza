43939bdb54436db3f785a4ba6cbe4a41ff54d941
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2025-05-16 16:07:09 +0100

    fix: improvement into typescript code

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,22 +5,24 @@ import { DOMAIN } from '@constants/domain';
 const PAGES = ['/blogs', '/contact', '/resume'];
 const NOW = new Date().toISOString().split('T')[0];
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const blogs: MetadataRoute.Sitemap = getBlogPosts().map((post) => ({
+type SiteMap = MetadataRoute.Sitemap[0];
+
+export default async function sitemap(): Promise<SiteMap[]> {
+  const blogs: SiteMap[] = getBlogPosts().map((post) => ({
     url: `${DOMAIN}/blogs/${post.metadata.slug}`,
     lastModified: post.metadata.lastModified,
     priority: 0.9,
     changeFrequency: 'daily',
   }));
 
-  const routes: MetadataRoute.Sitemap = PAGES.map((page) => ({
+  const routes: SiteMap[] = PAGES.map((page) => ({
     url: `${DOMAIN}${page}`,
     lastModified: NOW,
     priority: 0.8,
     changeFrequency: 'daily',
   }));
 
-  const home: MetadataRoute.Sitemap[0] = {
+  const home: SiteMap = {
     url: `${DOMAIN}`,
     lastModified: NOW,
     priority: 1,
--- src/type/polymorphic.type.ts
+++ src/type/polymorphic.type.ts
@@ -25,7 +25,7 @@ type OwnProps<E> = E extends ForwardRefComponent<any, infer P>
   : {};
 
 /**
- * Infers the JSX.IntrinsicElement if E is a ForwardRefExoticComponentWithAs
+ * Infers the React.JSX.IntrinsicElement if E is a ForwardRefExoticComponentWithAs
  */
 type IntrinsicElement<E> = E extends ForwardRefComponent<infer I, any>
   ? I
@@ -60,15 +60,15 @@ interface ForwardRefComponent<
    * props taking precendence.
    *
    * We explicitly avoid `React.ElementType` and manually narrow the prop types
-   * so that events are typed when using JSX.IntrinsicElements.
+   * so that events are typed when using React.JSX.IntrinsicElements.
    */
   <As = IntrinsicElementString>(
     props: As extends ''
-      ? { as: keyof JSX.IntrinsicElements }
+      ? { as: keyof React.JSX.IntrinsicElements }
       : As extends React.ComponentType<infer P>
       ? Merge<P, OwnProps & { as: As }>
-      : As extends keyof JSX.IntrinsicElements
-      ? Merge<JSX.IntrinsicElements[As], OwnProps & { as: As }>
+      : As extends keyof React.JSX.IntrinsicElements
+      ? Merge<React.JSX.IntrinsicElements[As], OwnProps & { as: As }>
       : never,
   ): React.ReactElement | null;
 }
@@ -79,11 +79,11 @@ interface MemoComponent<IntrinsicElementString, OwnProps = {}>
   > {
   <As = IntrinsicElementString>(
     props: As extends ''
-      ? { as: keyof JSX.IntrinsicElements }
+      ? { as: keyof React.JSX.IntrinsicElements }
       : As extends React.ComponentType<infer P>
       ? Merge<P, OwnProps & { as: As }>
-      : As extends keyof JSX.IntrinsicElements
-      ? Merge<JSX.IntrinsicElements[As], OwnProps & { as: As }>
+      : As extends keyof React.JSX.IntrinsicElements
+      ? Merge<React.JSX.IntrinsicElements[As], OwnProps & { as: As }>
       : never,
   ): React.ReactElement | null;
 }
